1ec53fd05650ad40cfc560ddad91d697b58ba6a6
DeviN1111111 <1100961@hr.nl>
2025-10-06 18:55:02 +0200

    Added unit test
    added unit test

---
+++ SupermarktApp.Tests/MapLogicTest.cs
@@ -0,0 +1,50 @@
+using NUnit.Framework;
+
+[TestFixture]
+public class MapLogicTest
+{
+    [Test]
+    public void CheckIfTheReturnStringContainsX()
+    {
+        string mapOutput = MapLogic.MapBuilder(5);
+
+        bool result = mapOutput.Contains("X");
+
+        Assert.IsTrue(result, "The map output should contain an 'X'.");
+    }
+
+    [Test]
+    public void CheckForCorrectOutput()
+    {
+
+        string expectedOutput = @"                               
+                                               ╔═══════╦══╦══╦══╦══╦═════╗
+                ╔════════════╦════════════╗    ║       ║  ║  ║  ║  ║     ║
+                ║            ║            ║    ║    ╔══╩══╩══╩══╩══╩═╗   ║
+                ║            ║            ║    ╠════╣                ║═══╣
+                ║            ║            ║    ║    ║                ║   ║
+                ║            ║            ║    ╠════╣                ║═══╣
+                ║            ║            ║    ║    ║                ║   ║
+                ╠════════ WC ╩ WC ═════════════╩════╝                ║═══╣
+                ║                                                    ║   ║
+                ║                                                    ╚═══╣
+                ║  ┌─┬─┬─┬─┬─┬─┬─┬─┬─┬─┬─┬─┬─┐                           ║
+                ║  │X│ │ │ │ │ │ │ │ │ │ │ │ │                           ║
+                ║  └─┴─┴─┴─┴─┴─┴─┴─┴─┴─┴─┴─┴─┘                           ║
+                ║  ┌─┬─┬─┬─┬─┬─┬─┬─┬─┬─┬─┬─┬─┐                           ║
+                ║  │ │ │ │ │ │ │ │ │ │ │ │ │ │                           ║
+                ║  └─┴─┴─┴─┴─┴─┴─┴─┴─┴─┴─┴─┴─┘                  ▓▓▓▓▓▓▓▓▓║
+                ║  ┌─┬─┬─┬─┬─┬─┬─┬─┬─┬─┬─┬─┬─┐                ▓▓▓▓▓▓▓▓▓▓▓║
+                ║  │ │ │ │ │ │ │ │ │ │ │ │ │ │               ▓▓▓▓▓       ║
+                ║  └─┴─┴─┴─┴─┴─┴─┴─┴─┴─┴─┴─┴─┘               ▓▓▓▓        ║
+                ║                                            ▓▓▓▓        ║
+                ║          CHECKOUT                          ▓▓▓▓        ║
+                ║    ░░   ░░   ░░   ░░   ░░                  ▓▓▓▓        ║
+                ║    ░░   ░░   ░░   ░░   ░░                  ▓▓▓▓  ╔══╗  ║
+                ║    ░░   ░░   ░░   ░░   ░░                  ▓▓▓▓  ║  ║  ║
+                ╚═════════════════════════════ ENTER ══ EXIT ══════╩══╩══╝";
+        string actualOutput = MapLogic.MapBuilder(1);
+
+        Assert.AreEqual(expectedOutput, actualOutput, "ERROR - The map output is not as expected.");
+    }
+}
