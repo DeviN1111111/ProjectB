2fbd1399933e36ef665ef3b0ae1ac7c17eedbae2
cMendes73 <cadu159@hotmail.com>
2025-10-10 16:52:53 +0200

    Enhance ShopDetails display with additional information panel

--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using NUnit.Framework.Constraints;
 using Spectre.Console;
 
 class Program
@@ -8,6 +10,5 @@ class Program
     static void Main()
     {
         ShopDetails.Show();
-       
     }
 }
--- SupermarktApp/Presentation/ShopDetails.cs
+++ SupermarktApp/Presentation/ShopDetails.cs
@@ -29,6 +29,18 @@ public static class ShopDetails
                 table.AddRow($"[bold #125e81]{day[0]}[/]", $"[#125e81]{day[1]}[/]", $"[#5dabcf]{OpeningHourSunday} - {ClosingHourSunday}[/]");
         }
         table.Border(TableBorder.Heavy);
-        AnsiConsole.Write(table);
+
+        var body = @"
+        Welcome to our supermarket — where freshness comes first.
+        Our bakery opens early with warm, freshly baked bread, and all our vegetables are kept perfectly cooled throughout the day.
+        Most restocking takes place in the evening, so the shelves are full and ready for you every morning.";
+        var panel = new Panel(body)
+        {
+            Border = BoxBorder.Heavy,
+            Header = new PanelHeader($"[bold #1B98E0]Info[/]").Centered()
+        };
+
+        var columns = new Columns(table, panel);
+        AnsiConsole.Write(columns);
     }
 }
