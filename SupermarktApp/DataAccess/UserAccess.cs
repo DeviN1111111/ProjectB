81f00589644dbedf62daea9abbf9d47cc72fa00f
Angel <nokhaiangel@gmail.com>
2025-11-16 22:18:12 +0100

    Check if email already exists in database

--- SupermarktApp/DataAccess/UserAccess.cs
+++ SupermarktApp/DataAccess/UserAccess.cs
@@ -76,4 +76,16 @@ public static class UserAccess
             WHERE ID = @ID", new { ID = userId });
     }
 
+    public static bool EmailExists(string email)
+    {
+        using var db = new SqliteConnection(ConnectionString);
+        int count = db.QuerySingleOrDefault<int>(
+            @"SELECT COUNT(1) 
+                FROM Users 
+                WHERE LOWER(Email) = LOWER(@Email)",
+            new { Email = email.Trim() }
+        );
+        // if there is a match found
+        return count > 0;
+    }
 }
