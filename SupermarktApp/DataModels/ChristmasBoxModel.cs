3b5b58764a31e395fd76f87d84552d663c618e09
Angel <nokhaiangel@gmail.com>
2025-12-12 20:12:05 +0100

    x-max box inherit product model

---
+++ SupermarktApp/DataModels/ChristmasBoxModel.cs
@@ -0,0 +1,10 @@
+public class ChristmasBoxModel : ProductModel
+{   // inherit from ProductModel
+    public const int MinimumProductsRequired = 3; // box must contain 3
+
+    public List<ProductModel> Products { get; set; } = new(); // list of products in xmas-box
+
+    public decimal TotalProductsValue =>
+        // calculate total price content box
+        Products.Sum(p => (decimal)p.Price);  
+}
