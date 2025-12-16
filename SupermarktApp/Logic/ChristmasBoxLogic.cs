9e6b65adc7c5b27dcc8bcf75e01b626625c9b778
Angel <nokhaiangel@gmail.com>
2025-12-16 15:34:47 +0100

    .

--- SupermarktApp/Logic/ChristmasBoxLogic.cs
+++ SupermarktApp/Logic/ChristmasBoxLogic.cs
@@ -19,7 +19,16 @@ public static class ChristmasBoxLogic
     // Validates if a Christmas box meets the business rules
     public static bool IsValidBox(ChristmasBoxModel box)
     {
-        // TODO: Check minimum products and price rules
-        return false;
+        if (box == null)
+        {
+            return false;
+        }
+
+        if (!box.HasMinimumProducts())
+        {
+            return false;
+        }
+
+        return box.TotalProductsValue >= (decimal)box.Price && box.Price > 0;
     }
 }
