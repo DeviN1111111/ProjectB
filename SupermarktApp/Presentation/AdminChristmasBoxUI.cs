a63a9efd7e8da892899ffc416c98483e22e1da81
Angel <nokhaiangel@gmail.com>
2026-01-06 13:00:05 +0100

    fix admin promt

--- SupermarktApp/Presentation/AdminChristmasBoxUI.cs
+++ SupermarktApp/Presentation/AdminChristmasBoxUI.cs
@@ -1,32 +1,39 @@
 using Spectre.Console;
 using System.Linq;
+using System;
+using System.Collections.Generic;
 
 public static class AdminChristmasBoxUI
 {
     public static void Show()
     {
-        Console.Clear();
-        Utils.PrintTitle("Christmas Box Admin");
-
-        var products = ProductLogic.GetAllProducts();
-
-        // Multi-select products
-        var selectedProducts = Utils.CreateMultiSelectionPrompt(
-            products,
-            "[white]Select products for Christmas boxes[/]",
-            p => $"{p.ID} - {p.Name} ({p.Category})"
-            // p => $"{p.ID} - {p.Name} (Eligible: {p.IsChristmasBoxItem})"
-        );
-
-        // Add items to christmas category
-        foreach (var product in selectedProducts)
+        while (true)
         {
-            product.IsChristmasBoxItem = !product.IsChristmasBoxItem;
-            ProductLogic.UpdateProduct(product);
+            Console.Clear();
+            Utils.PrintTitle("Christmas Box Admin");
         }
+        
+        
 
-        // AnsiConsole.MarkupLine("[green]Product updated successfully![/]");
-        AnsiConsole.MarkupLine("Press [green]ENTER[/] to continue");
-        Console.ReadKey();
+        // var products = ProductLogic.GetAllProducts();
+
+        // // Multi-select products
+        // var selectedProducts = Utils.CreateMultiSelectionPrompt(
+        //     products,
+        //     "[white]Select products for Christmas boxes[/]",
+        //     p => $"{p.ID} - {p.Name} ({p.Category})"
+        //     // p => $"{p.ID} - {p.Name} (Eligible: {p.IsChristmasBoxItem})"
+        // );
+
+        // // Add items to christmas category
+        // foreach (var product in selectedProducts)
+        // {
+        //     product.IsChristmasBoxItem = !product.IsChristmasBoxItem;
+        //     ProductLogic.UpdateProduct(product);
+        // }
+
+        // // AnsiConsole.MarkupLine("[green]Product updated successfully![/]");
+        // AnsiConsole.MarkupLine("Press [green]ENTER[/] to continue");
+        // Console.ReadKey();
     }
 }
